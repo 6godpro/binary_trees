d07cc911de0559dd21d1a2ba0c2083e7ffde9fea
6godpro <erickson5115@gmail.com>
2023-05-30 13:18:52 +0100

    (fix)-> fixed styling guide error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,10 +11,10 @@ int get_max(int x, int y);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        int left = 0, right = 0;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	left = get_height(tree->left);
 	right = get_height(tree->right);
