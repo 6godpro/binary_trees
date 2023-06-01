8ebfa37af161ef5163e46f5a015782abdc9b7bdd
6godpro <erickson5115@gmail.com>
2023-06-01 23:04:46 +0100

    Solution for task 26. BST - Array to BST

---
+++ 112-array_to_bst.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+
+/**
+ * array_to_bst - Creates a BST from an array.
+ * @array: Array from which the tree is to be built.
+ * @size: The size of the array.
+ *
+ * Return: On success - A pointer to the root node of the tree.
+ *         Otherwise - NULL.
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	bst_t *root = NULL, *node;
+	size_t i = 0;
+
+	if (array == NULL || size <= 0)
+		return (NULL);
+
+	while (i < size)
+	{
+		node = bst_insert(&root, array[i++]);
+		if (node == NULL)
+			return (NULL);
+	}
+	return (root);
+}
